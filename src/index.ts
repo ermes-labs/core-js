b10d082b7849add90bfcff8c31260c20edcf1d39
Paolo <paololongo.git@gmail.com>
2024-04-24 14:28:00 +0200

    Fix exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export { type SessionToken } from "./api/sessionToken";
 
-export { type SessionCommands } from "./commands/session";
-
 export { type GeoCoordinates } from "./infrastructure/geo-coordinates";
 export { type SessionLocation } from "./infrastructure/session-location";
